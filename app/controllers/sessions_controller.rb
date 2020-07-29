fc4cc66779772c2ccea07e38ffff24e32849b2ff
connorworthen <connorw220@protonmail.com>
2020-07-29 16:38:15 -0700

    initial commits signup/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,27 @@
 class SessionsController < ApplicationController
-  def new
-  end
 
-  def create
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+  
+  def new
   end
 
   def login
   end
 
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      sessions[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
+  end
+
   def welcome
   end
+
+  def page_requires_login
+  end
+
 end
