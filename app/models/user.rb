757d21b4c2dde9b40193fcf094bf7d44dc668e38
connorworthen <connorw220@protonmail.com>
2020-07-29 16:27:37 -0700

    sessions controller

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class Model < ActiveRecord::Base
+  has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
